90a89b39bd81ce74b0a3b1d5a992f70fa5f6c6b4
Jeisen515 <jeisen.soler@upr.edu>
2023-10-24 18:40:57 -0400

    test #69 of the dashboard velocity

--- motor_velocity.py
+++ motor_velocity.py
@@ -1,42 +1,41 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO pin
-GPIO_PIN = 27  # Change this to your GPIO pin
+def get_velocity(GPIO_PIN):
+    # Constants for calculation
+    PULSES_PER_ROTATION = 48
+    WHEEL_RADIUS_M = 0.24
+    METERS_TO_MILES = 0.000621371  # Conversion factor for meters to miles
 
-# Constants for calculation
-PULSES_PER_ROTATION = 48
-WHEEL_RADIUS_M = 0.24
-METERS_TO_MILES = 0.000621371  # Conversion factor for meters to miles
+    # Initialize GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Variables for pulse counting
+    pulse_count = 0
+    start_time = time.time()
 
-# Variables for pulse counting
-pulse_count = 0
-start_time = time.time()
+    try:
+        # while True:
+        # Wait for a rising edge
+        GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
+        pulse_count += 1
 
-try:
-    # while True:
-    # Wait for a rising edge
-    GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
-    pulse_count += 1
+        # Calculate time elapsed
+        end_time = time.time()
+        elapsed_time = end_time - start_time
 
-    # Calculate time elapsed
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+        # Reset the count and time if a second has passed
+        if elapsed_time >= 1.0:
+            speed_mph = (pulse_count / PULSES_PER_ROTATION) * (WHEEL_RADIUS_M)
+            velocity = speed_mph * METERS_TO_MILES * 1609.34 # convert to meters per second
+            return velocity
 
-    # Reset the count and time if a second has passed
-    if elapsed_time >= 1.0:
-        speed_mph = (pulse_count / PULSES_PER_ROTATION) * (WHEEL_RADIUS_M)
-        print(f"Speed: {speed_mph:.2f} mph")
+            pulse_count = 0
+            start_time = time.time()
 
-        pulse_count = 0
-        start_time = time.time()
+    except KeyboardInterrupt:
+        print("Measurement stopped by user")
 
-except KeyboardInterrupt:
-    print("Measurement stopped by user")
-
-finally:
-    GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
