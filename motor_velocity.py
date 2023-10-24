57983188746f265ea576848c17a02bec5a88eb42
Gabriel Roman <89166066+gabrielroman1107@users.noreply.github.com>
2023-10-24 19:50:53 -0400

    Update motor_velocity.py jejej

--- motor_velocity.py
+++ motor_velocity.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
+import asyncio
 import time
+import websockets
 
 # Define GPIO pin
 GPIO_PIN = 27  # Change this to your GPIO pin
@@ -7,36 +9,36 @@ GPIO_PIN = 27  # Change this to your GPIO pin
 # Constants for calculation
 PULSES_PER_ROTATION = 48
 WHEEL_RADIUS_M = 0.24
-METERS_TO_MILES = 0.000621371  # Conversion factor for meters to miles
-
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Variables for pulse counting
 pulse_count = 0
 start_time = time.time()
 
-try:
+async def speed_measurement(websocket, path):
     while True:
-        # Wait for a rising edge
-        GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
-        pulse_count += 1
+        await asyncio.sleep(1)  # Update speed every second
 
-        # Calculate time elapsed
-        end_time = time.time()
-        elapsed_time = end_time - start_time
+        speed_mph = (pulse_count / PULSES_PER_ROTATION) * WHEEL_RADIUS_M * 2.237  # Convert to mph
+        await websocket.send(f"Speed: {speed_mph:.2f} mph")
 
-        # Reset the count and time if a second has passed
-        if elapsed_time >= 1.0:
-            speed_mph = (pulse_count / PULSES_PER_ROTATION) * (WHEEL_RADIUS_M)
-            print(f"Speed: {speed_mph:.2f} mph")
+def gpio_callback(channel):
+    global pulse_count
+    pulse_count += 1
 
-            pulse_count = 0
-            start_time = time.time()
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=gpio_callback)
 
-except KeyboardInterrupt:
-    print("Measurement stopped by user")
+    start_server = websockets.serve(speed_measurement, "0.0.0.0", 5678)
 
-finally:
-    GPIO.cleanup()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_server)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Measurement stopped by user")
+
+    finally:
+        GPIO.cleanup()
