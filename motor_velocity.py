a5f4ed94f788629daa84187eaf1b58e19993434a
Gabriel Roman <89166066+gabrielroman1107@users.noreply.github.com>
2023-10-24 20:14:14 -0400

    Update motor_velocity.py
    back to old commit

--- motor_velocity.py
+++ motor_velocity.py
@@ -1,44 +1,43 @@
 import RPi.GPIO as GPIO
-import asyncio
 import time
-import websockets
-
-# Define GPIO pin
-GPIO_PIN = 27  # Change this to your GPIO pin
 
 # Constants for calculation
 PULSES_PER_ROTATION = 48
 WHEEL_RADIUS_M = 0.24
+METERS_TO_MILES = 0.000621371  # Conversion factor for meters to miles
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Variables for pulse counting
 pulse_count = 0
 start_time = time.time()
 
-async def speed_measurement(websocket, path):
-    while True:
-        await asyncio.sleep(1)  # Update speed every second
-
-        speed_mph = (pulse_count / PULSES_PER_ROTATION) * WHEEL_RADIUS_M * 2.237  # Convert to mph
-        await websocket.send(f"Speed: {speed_mph:.2f} mph")
-
-def gpio_callback(channel):
-    global pulse_count
-    pulse_count += 1
-
-if __name__ == '__main__':
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=gpio_callback)
-
-    start_server = websockets.serve(speed_measurement, "0.0.0.0", 5678)
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(start_server)
-
+while True:
     try:
-        loop.run_forever()
+        # Wait for a rising edge
+        GPIO.wait_for_edge(GPIO_PIN, GPIO.RISING)
+        pulse_count += 1
+
+        # Calculate time elapsed
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        # Reset the count and time if a second has passed
+        if elapsed_time >= 1.0:
+            speed_mph = (pulse_count / PULSES_PER_ROTATION) * WHEEL_RADIUS_M
+            velocity = (
+                speed_mph * METERS_TO_MILES * 1609.34
+            )  # convert to meters per second
+
+            return str(velocity)  # Return the velocity as a string
+
+            pulse_count = 0
+            start_time = time.time()
+
     except KeyboardInterrupt:
-        print("Measurement stopped by user")
+        print("Measurement stopped by the user")
 
     finally:
         GPIO.cleanup()
